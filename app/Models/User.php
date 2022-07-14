0e2368240c62c88ddfd4e3064594eddf377941c4
Gabriel Pires <gabrielfpp47@gmail.com>
2022-07-13 22:40:54 -0300

    creating the relationship of account to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,11 +12,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'phone_number',
@@ -26,22 +21,17 @@ class User extends Authenticatable
         'email',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function account()
+    {
+        return $this->hasOne(Account::class);
+    }
 }
